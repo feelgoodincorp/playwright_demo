b825336cfbd2bd8edec51e68d8985e2affb09168
feelgoodincorp <newuser96@ukr.net>
2025-05-01 22:44:53 +0300

    init

---
+++ tests/example.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+// test('has title', async ({ page }) => {
+//     await page.goto('https://playwright.dev/');
+//
+//     // Ожидаем, что заголовок содержит подстроку
+//     await expect(page).toHaveTitle(/Playwright/);
+// });
+//
+// test('get started link', async ({ page }) => {
+//     await page.goto('https://playwright.dev/');
+//
+//     // Клик по ссылке "Get started"
+//     await page.getByRole('link', { name: 'Get started' }).click();
+//
+//     // Ожидаем переход на страницу документации
+//     await expect(page).toHaveURL(/.*docs\/intro/);
+// });
+
+test('test 1', async ({ page }) => {
+    await page.goto('https://google.com')
+    await expect(page).toHaveTitle('Google')
+});
